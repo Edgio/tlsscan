8238ec239e9f3d77e2a21e3f3421e7e89c613d4a
Marcus Hildum <airencracken@users.noreply.github.com>
2020-12-02 22:33:33 -0800

    prefix option (#4)

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ usage() {
 $0: [hs]
 	-h display some help, you know, this
 	-s shallow clone, useful for faster builds
+	-p install prefix
 
 -s doesn't work with git prior to 2.8 (e.g. xenial)
 EOM
@@ -19,8 +20,8 @@ EOM
 # ------------------------------------------------------------------------------
 check_req() {
 	which cmake g++ make || {
-	    echo "Failed to find required build packages. Please install with: sudo apt-get install cmake make g++"
-	    exit 1
+		echo "Failed to find required build packages. Please install with: sudo apt-get install cmake make g++"
+		exit 1
 	}
 }
 # ------------------------------------------------------------------------------
@@ -28,8 +29,8 @@ check_req() {
 # ------------------------------------------------------------------------------
 submod_sync() {
 	git submodule sync || {
-	    echo "FAILED TO SYNC SUBMODULES"
-	    exit 1
+		echo "FAILED TO SYNC SUBMODULES"
+		exit 1
 	}
 }
 # ------------------------------------------------------------------------------
@@ -38,13 +39,13 @@ submod_sync() {
 submod_update() {
 	if [[ "$1" == "true" ]]; then
 		git submodule update -f --init --depth 1 || {
-		    echo "FAILED TO UPDATE TO LATEST (SHALLOW) SUBMODULES"
-		    exit 1
+			echo "FAILED TO UPDATE TO LATEST (SHALLOW) SUBMODULES"
+			exit 1
 		}
 	else
 		git submodule update -f --init || {
-		    echo "FAILED TO UPDATE TO LATEST SUBMODULES"
-		    exit 1
+			echo "FAILED TO UPDATE TO LATEST SUBMODULES"
+			exit 1
 		}
 	fi
 }
@@ -60,7 +61,7 @@ main() {
 	pushd build && \
 		cmake ../ \
 		-DBUILD_SYMBOLS=ON \
-		-DCMAKE_INSTALL_PREFIX=/usr && \
+		-DCMAKE_INSTALL_PREFIX=${install_prefix} && \
 		make -j${NPROC} && \
 		umask 0022 && chmod -R a+rX . && \
 		make package && \
@@ -70,15 +71,19 @@ main() {
 
 #set shallow to false
 shallow_clone="false"
+install_prefix="/usr"
 
 #parse options
-while getopts "hs" opts; do
+while getopts ":hsp:" opts; do
 	case "${opts}" in
 		h)
 			usage
 			;;
 		s)
 			shallow_clone="true"
+			;;
+		p)
+			install_prefix="${OPTARG}"
 			;;
 		*)
 			usage
